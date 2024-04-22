76fe2c83ecc47a6e7237cd100884d43a8d73ce6b
EdiedRamos <edieisa22@gmail.com>
2024-04-22 17:49:13 -0500

    chore: wrong and right icon logic added

--- src/components/Molecules/Options/Options.tsx
+++ src/components/Molecules/Options/Options.tsx
@@ -1,18 +1,29 @@
 import { Button } from "@/components/Atoms";
 
+import { CheckRound, CloseRound } from "@/assets";
+
 import "./Options.scss";
 
 type OptionsProps = {
   className?: string;
 };
 
+const renderCorrect = (condition: boolean) =>
+  condition && <img src={CheckRound} />;
+
+const renderIncorrect = (condition: boolean) =>
+  condition && <img src={CloseRound} />;
+
 export const Options = ({ className }: OptionsProps) => {
   const options = ["Sweden", "Vietnam", "Malaysia", "Austria"];
 
   return (
     <div className={`options ${className}`}>
-      {options.map((option) => (
-        <Button key={option}>{option}</Button>
+      {options.map((option, index) => (
+        <Button className={index === 1 ? "button--selected" : ""} key={option}>
+          {option} {renderCorrect(index === 3)}
+          {renderIncorrect(index === 1)}
+        </Button>
       ))}
     </div>
   );
