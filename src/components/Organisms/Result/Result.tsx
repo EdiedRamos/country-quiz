2bc9dbc300c8a0552a469064ff635253b3a2f850
EdiedRamos <edieisa22@gmail.com>
2024-04-22 02:28:33 -0500

    style: result basic styles added

--- src/components/Organisms/Result/Result.tsx
+++ src/components/Organisms/Result/Result.tsx
@@ -1,13 +1,15 @@
 import { CongratsImg } from "@/assets";
 import { Button } from "@/components/Atoms";
 
+import "./Result.scss";
+
 export const Result = () => {
   return (
-    <section>
+    <section className="result">
       <img src={CongratsImg} alt="Congrats icon" />
       <h2>Congrats! You completed the quiz.</h2>
       <p>You answer 4/10 correctly</p>
-      <Button>Play again</Button>
+      <Button className="button--selected">Play again</Button>
     </section>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-import "./general/styles/index.scss";
+// import "./general/styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
