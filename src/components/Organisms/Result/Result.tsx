cd0f15ff63989ebce9755e0510604176e5060524
EdiedRamos <edieisa22@gmail.com>
2024-04-22 23:09:47 -0500

    feat: set the correct score

--- src/components/Organisms/Result/Result.tsx
+++ src/components/Organisms/Result/Result.tsx
@@ -2,13 +2,18 @@ import { CongratsImg } from "@/assets";
 import { Button } from "@/components/Atoms";
 
 import "./Result.scss";
+import { useQuiz } from "@/hooks/useQuiz";
 
 export const Result = () => {
+  const quizState = useQuiz();
+
   return (
     <section className="result">
       <img src={CongratsImg} alt="Congrats icon" />
       <h2 className="result__congrats">Congrats! You completed the quiz.</h2>
-      <p className="result__result">You answer 4/10 correctly</p>
+      <p className="result__result">
+        You answer {quizState?.score}/10 correctly
+      </p>
       <Button className="button--selected result__play-again">
         Play again
       </Button>
